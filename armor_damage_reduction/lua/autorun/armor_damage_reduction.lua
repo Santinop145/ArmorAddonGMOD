d9791d737636d990d29da58085b01c879da23b2a
Santinop145 <143087754+Santinop145@users.noreply.github.com>
2024-10-17 12:43:07 -0300

    Add files via upload

--- armor_damage_reduction/lua/autorun/armor_damage_reduction.lua
+++ armor_damage_reduction/lua/autorun/armor_damage_reduction.lua
@@ -1,14 +1,31 @@
 if SERVER then
+    -- Define the main function and take ent and dmg from the hook.
     local function ArmorReduceDamage(ent, dmg)
-    if(ent:IsPlayer() and dmg:GetDamage() > 0) then
+    -- Check if targeted entity is a player, then check if it received more than 0 damage.
+        if(ent:IsPlayer() and dmg:GetDamage() > 0) then
+            -- Define the targeted entity as a player variable.
             ply = ent
+            -- Get current armor.
             local CurrentArmor = ply:Armor()
+            -- Get current max armor.
             local CurrentMaxArmor = ply:GetMaxArmor()
+            -- Get current armor percentage.
             local ArmorPercentage = CurrentArmor / CurrentMaxArmor
 
+            -- Apply global damage reduction.
             if(ArmorPercentage > 0) then
                 dmg:ScaleDamage(0.8)
             end
+
+            --[[
+            Apply reduction to damage after certain percentages. Default: 
+            75% damage reduction when above 75 armor
+            50% damage reduction when above 50 armor
+            25% damage reduction when above 25 armor
+            20% damage reduction overall (applies before armor reduction)
+            Also apply sound effects on player damage to give a sound cue to damage reduction.
+            --]]
+
 
             if(ArmorPercentage >= 0.75) then
                 dmg:ScaleDamage(0.25)
@@ -37,14 +54,46 @@ if SERVER then
                 ply:EmitSound("physics/flesh/flesh_squishy_impact_hard3.wav", 45, 100, 1, CHAN_AUTO)
             end
 
+            -- Check if the player has armor, and if it's below the max armor, then add 1 armor point on damage.
+            -- If player's health is below 50, whenever receiving damage, add 1 health point and add a sound cue.
             if(CurrentArmor > 0 and CurrentArmor < CurrentMaxArmor) then
                 ply:SetArmor(CurrentArmor + 1)
                 if(ply:Health() < 50)then
-                    ply:SetHealth(ply:Health() + 1)
+                    ply:SetHealth(ply:Health() + 2)
                     ply:EmitSound("items/medshot4.wav", 30, 50, 1, CHAN_AUTO)
+                    ply:EmitSound("player/pl_burnpain1.wav", 50, 110, 1, CHAN_AUTO)
                 end
             end
         end
     end
+    -- Add the function to the EntityTakeDamage hook so it is called whenever an entity takes damage.
+    local function MorphineShot(ent, dmg)
+        -- Check if targeted entity is a player and if damage is above 10.
+        if(ent:IsPlayer() and dmg:GetDamage > 10) then
+            -- Define entity as a player variable.
+            ply = ent
+            -- Get health and max health to create a percentage
+            local CurrentHealth = ply:Health()
+            local CurrentMaxHealth = ply:GetMaxHealth()
+            local HealthPercentage = CurrentHealth / CurrentMaxHealth
+            -- Define the amount of health morphine will give, the time delay and the cooldown variable
+            local MorphineHealthAmount = 0
+            local Delay = 10
+            local MorphineCooldown = CurTime()
+
+            --[[ Check if player has less than 50% health. If true, check if MorphineCooldown is above the server's current time.
+            If true, set the Morphine health amount as an overly complicated formula for no reason at all because I wanted to have a funny number
+            as a healing value. Why not?
+            Afterwards, add the morphine's health amount to the player's health.
+            Change the cooldown]]
+            if(HealthPercentage < 0.5 && MorphineCooldown => CurTime()) then
+                MorphineHealthAmount = CurrentHealth + (CurrentHealth / (HealthPercentage * (HealthPercentage + 1.5)))
+                ply:Health() = CurrentHealth + MorphineHealthAmount
+                ply:EmitSound("HL1/fvox/morphine_shot.wav", 100, 100, 1, CHAN_AUTO)
+                MorphineCooldown = CurTime() + Delay
+            end
+        end
+    end
     hook.Add("EntityTakeDamage", "ArmorReduceDamage", ArmorReduceDamage)
+    hook.Add("EntityTakeDamage", "AdministerMorphine", MorphineShot)
 end
